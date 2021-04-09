0a8cd4773492f694a639a5ceff4dc6aceb257a8a
Noah Hüsser <yatekii@yatekii.ch>
2021-04-09 13:29:42 +0200

    Flush stdout properly; Fix race on process completioN

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ mod tera_engine;
 
 use std::process::Stdio;
 
-use chrono::{Datelike, TimeZone, Timelike, Utc};
-use rocket::{get, http::ContentType, launch, post, response::Redirect, routes, Response, Rocket};
-use rocket_contrib::{json::Json, serve::StaticFiles};
+use rocket::{get, http::ContentType, launch, response::Redirect, routes, Response, Rocket};
+use rocket_contrib::serve::StaticFiles;
 use serde::{Deserialize, Serialize};
 use tokio::{
     io::{AsyncWriteExt, BufWriter},
@@ -17,14 +16,16 @@ fn index() -> Redirect {
 }
 
 #[get("/get/<hash>")]
-async fn get<'a>(hash: String) -> Result<Response<'a>, ()> {
+async fn get<'a>(hash: String) -> Result<Response<'a>, tokio::io::Error> {
     let json = base64::decode(hash).unwrap();
     let json = serde_json::from_slice(&json).unwrap();
-    let stdout = generate_pdf(&json).await;
+    let stdout = generate_pdf(&json).await?;
+    println!("{}", stdout.0);
     let response = rocket::Response::build()
         .header(ContentType::new("application", "pdf"))
-        .streamed_body(stdout)
+        .streamed_body(stdout.1)
         .ok();
+
     return response;
 }
 
@@ -52,7 +53,9 @@ pub struct Position {
     vat_must: bool,
 }
 
-async fn generate_pdf(data: &GenerateData) -> ChildStdout {
+async fn generate_pdf(
+    data: &GenerateData,
+) -> Result<(std::process::ExitStatus, ChildStdout), tokio::io::Error> {
     println!("{:?}", data);
     // here "ipinfo::Response" need also be changed to "ip2asn::Response" for free api calls
     let mut context = tera::Context::new();
@@ -62,7 +65,7 @@ async fn generate_pdf(data: &GenerateData) -> ChildStdout {
         .unwrap();
 
     // execute weasyprint to generate pdf
-    let weasyprint = Command::new("python3")
+    let mut weasyprint = Command::new("python3")
         .args(&["-m", "weasyprint"])
         .args(&["-f", "pdf"])
         .args(&["-e", "utf8"])
@@ -74,11 +77,12 @@ async fn generate_pdf(data: &GenerateData) -> ChildStdout {
         .spawn()
         .expect("failed to execute process");
 
-    let mut stdin = weasyprint.stdin.unwrap();
-    let stdout = weasyprint.stdout.unwrap();
-    let mut writer = BufWriter::new(&mut stdin);
+    if let Some(mut stdin) = weasyprint.stdin.as_mut() {
+        let mut writer = BufWriter::new(&mut stdin);
+        writer.write_all(&render.into_bytes()).await?;
+        writer.flush().await?;
+    }
 
-    writer.write_all(&render.into_bytes()).await.unwrap();
-
-    stdout
+    let exit_status = weasyprint.wait().await?;
+    Ok((exit_status, weasyprint.stdout.expect("This is a bug.")))
 }
